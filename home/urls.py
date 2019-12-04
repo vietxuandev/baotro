0edd681b5e41f87003b13029c9ed8a8915d3da72
Viet Xuan <vietxuandev@gmail.com>
2019-12-04 11:21:29 +0700

    fix all

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from home import views
+
+app_name = 'home'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('content', views.content, name='content'),
+]
