dbfa8f5d6275584927a63d43ff52fecf677b57ff
Viet Xuan <vietxuandev@gmail.com>
2020-02-16 21:29:01 +0700

    update

--- blog/models.py
+++ blog/models.py
@@ -8,12 +8,19 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    type_choices = [
+        ('0', 'Tin trung tâm'),
+        ('1', 'Tin trong tỉnh'),
+        ('2', 'Hoàn cảnh cần giúp đỡ'),
+        ('3', 'Thông tin chỉ đạo, điều hành'),
+    ]
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     image = models.ImageField(blank=True)
     video_link = models.URLField(max_length=2000, blank=True)
     description = models.CharField(max_length=500, blank=True)
     content = RichTextField(blank=True, null=True)
+    type = models.CharField(default='0', max_length=100, choices=type_choices)
     created_date = models.DateTimeField(default=timezone.now)
 
     # published_date = models.DateTimeField(blank=True, null=True)
